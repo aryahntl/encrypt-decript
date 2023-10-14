726949d27d3a81a315715ba9f40d016ac9bcceda
Himanshu Arya <arya.h.ntl@gmail.com>
2023-10-14 23:34:07 +0530

    adding repation

--- project4.py
+++ project4.py
@@ -3,24 +3,36 @@ alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r
 def encryption(plain_text,shift_key):
     cipher_text=""
     for char in plain_text:
-        position=alphabet.index(char)
-        new_position=(position+shift_key)%26
-        cipher_text +=alphabet[new_position]
+        if char in alphabet:
+            position=alphabet.index(char)
+            new_position=(position+shift_key)%26
+            cipher_text +=alphabet[new_position]
+        else:
+            cipher_text+=char
     print(f"here's the text after encryption:{cipher_text}")
 
 
 def decryption(cipher_text,shift_key):
     plain_text=""
     for char in cipher_text:
-        position=alphabet.index(char)
-        new_position=(position-shift_key)%26
-        plain_text +=alphabet[new_position]
+        if char in alphabet:                
+            position=alphabet.index(char)
+            new_position=(position-shift_key)%26
+            plain_text +=alphabet[new_position]
+        else:
+            cipher_text+=char
     print(f"here's the text after encryption:{plain_text}")
 
-what_to_do=input('type "encrypt" for encryption, type"decrypt" for decryption: ')
-text=input('type your message: ')
-shift=int(input("Enter shift key: "))
-if what_to_do=="encrypt":
-    encryption(plain_text=text,shift_key=shift)
-elif what_to_do=="decrypt":
-    decryption(cipher_text=text,shift_key=shift)
+wanna_end=False
+while not wanna_end:
+    what_to_do=input('type "encrypt" for encryption, type"decrypt" for decryption: ')
+    text=input('type your message: ')
+    shift=int(input("Enter shift key: "))
+    if what_to_do=="encrypt":
+        encryption(plain_text=text,shift_key=shift)
+    elif what_to_do=="decrypt":
+        decryption(cipher_text=text,shift_key=shift)
+    play_again=input("type 'yes' to continue, type 'no' to exit: ")
+    if play_again=="no":
+        wanna_end=True
+        print("have a nice day! bye.")
