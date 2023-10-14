9cb2540fa4f3b19c680c858dc31f224fe4e39774
Himanshu Arya <arya.h.ntl@gmail.com>
2023-10-14 23:16:35 +0530

    added decryption

--- project4.py
+++ project4.py
@@ -4,14 +4,23 @@ def encryption(plain_text,shift_key):
     cipher_text=""
     for char in plain_text:
         position=alphabet.index(char)
-        new_position=position+shift_key
-        cipher_text+=alphabet[new_position]
+        new_position=(position+shift_key)%26
+        cipher_text +=alphabet[new_position]
     print(f"here's the text after encryption:{cipher_text}")
+
+
+def decryption(cipher_text,shift_key):
+    plain_text=""
+    for char in cipher_text:
+        position=alphabet.index(char)
+        new_position=(position-shift_key)%26
+        plain_text +=alphabet[new_position]
+    print(f"here's the text after encryption:{plain_text}")
 
 what_to_do=input('type "encrypt" for encryption, type"decrypt" for decryption: ')
 text=input('type your message: ')
 shift=int(input("Enter shift key: "))
 if what_to_do=="encrypt":
     encryption(plain_text=text,shift_key=shift)
-# elif what_to_do=="decrypt":
-#     decryption(text,shift)
+elif what_to_do=="decrypt":
+    decryption(cipher_text=text,shift_key=shift)
