04c0a4c020451034e40480dec08e6db0ac53f224
Himanshu Arya <arya.h.ntl@gmail.com>
2023-10-14 22:56:48 +0530

    add initial

---
+++ project4.py
@@ -0,0 +1,17 @@
+alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+def encryption(plain_text,shift_key):
+    cipher_text=""
+    for char in plain_text:
+        position=alphabet.index(char)
+        new_position=position+shift_key
+        cipher_text+=alphabet[new_position]
+    print(f"here's the text after encryption:{cipher_text}")
+
+what_to_do=input('type "encrypt" for encryption, type"decrypt" for decryption: ')
+text=input('type your message: ')
+shift=int(input("Enter shift key: "))
+if what_to_do=="encrypt":
+    encryption(plain_text=text,shift_key=shift)
+# elif what_to_do=="decrypt":
+#     decryption(text,shift)
